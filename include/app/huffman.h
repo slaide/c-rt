486f676a80ca28ba0a966cfa2aee6389a744e11b
slaide <patrick_hennig@fastmail.com>
2023-08-09 13:43:42 +0200

    minor cleanup

--- include/app/huffman.h
+++ include/app/huffman.h
@@ -168,92 +168,11 @@ typedef struct BitStream{
 void BitStream_new(BitStream* stream,void* data);
 
 /**
- * @brief get next n bits from stream
- * n must not be larger than 57. the internal bit buffer is automatically filled if it was not big enough at function start.
- * this function does NOT advance the internal state, i.e. repeated calls to this function with the same arguments will yield the same result.
- * call BitStream_advance to manually advance the stream.
- * @param stream 
- * @param n_bits 
- * @return int 
- */
-/*FORCE_INLINE
-static inline uint64_t BitStream_get_bits(BitStream* stream,uint8_t n_bits);
-
-FORCE_INLINE
-static inline void BitStream_advance_unsafe(BitStream* stream,uint8_t n_bits);*/
-/**
  * @brief advance stream
  * 
  * @param stream 
  * @param n_bits 
  */
-[[clang::always_inline,gnu::flatten]]
-static inline void BitStream_advance(BitStream* const restrict stream,const uint8_t n_bits);
-
-/**
- * @brief fill internal bit buffer (used for fast lookup)
- * this function is called automatically (internally) when required
- * @param stream 
- */
-[[gnu::hot]]
-static inline void BitStream_fill_buffer(
-    BitStream* const restrict stream
-){
-    const uint64_t num_bytes_missing=7-stream->buffer_bits_filled/8;
-
-    uint64_t new_bytes=0;
-    for(uint64_t i=0;i<num_bytes_missing;i++){
-        const uint64_t index=stream->next_data_index+i;
-        const uint64_t next_byte=stream->data[index];
-
-        const uint64_t shift_by=(7-i)*8;
-        new_bytes |= next_byte << shift_by;
-
-        if(stream->data[index]==0xFF && stream->data[index+1]==0){
-            stream->next_data_index++;
-        }
-    }
-    stream->buffer|=new_bytes>>stream->buffer_bits_filled;
-    stream->buffer_bits_filled+=num_bytes_missing*8;
-    stream->next_data_index+=num_bytes_missing;
-}
-/**
- * @brief ensure that the bitstream has at least n bits cached
- * this function will fill the internal cache if the cache does not already have sufficient number of bits
- * @param stream 
- * @param n_bits 
- */
-[[clang::always_inline,gnu::flatten]]
-static inline void BitStream_ensure_filled(
-    BitStream* const restrict stream,
-    const uint8_t n_bits
-){
-    if(stream->buffer_bits_filled<n_bits){
-        BitStream_fill_buffer(stream);
-    }
-}
-
-[[clang::always_inline,gnu::flatten]]
-static inline uint64_t BitStream_get_bits_unsafe(
-    const BitStream* const restrict stream,
-    uint8_t n_bits
-){
-    uint64_t ret=stream->buffer>>(64-n_bits);
-    return ret;
-}
-
-[[clang::always_inline,gnu::flatten]]
-static inline uint64_t BitStream_get_bits(
-    BitStream* const restrict stream,
-    const uint8_t n_bits
-){
-    BitStream_ensure_filled(stream, n_bits);
-
-    uint64_t ret=BitStream_get_bits_unsafe(stream, n_bits);
-
-    return ret;
-}
-
 [[clang::always_inline,gnu::flatten]]
 static inline void BitStream_advance_unsafe(
     BitStream* const restrict stream,
@@ -276,14 +195,85 @@ static inline void BitStream_advance(
     BitStream_advance_unsafe(stream, n_bits);
 }
 
+/**
+ * @brief fill internal bit buffer (used for fast lookup)
+ * this function is called automatically (internally) when required
+ * @param stream 
+ */
+[[gnu::hot,gnu::flatten]]
+static inline void BitStream_fill_buffer(
+    BitStream* const restrict stream
+){
+    const uint64_t num_bytes_missing = (64-stream->buffer_bits_filled)/8;
+
+    uint64_t new_bytes=0;
+    for(uint64_t i=0; i<num_bytes_missing; i++){
+        const uint64_t next_byte = stream->data[stream->next_data_index++];
+
+        const uint64_t shift_by = (7-i)*8;
+        new_bytes |= next_byte << shift_by;
+
+        if(next_byte==0xFF && stream->data[stream->next_data_index]==0){
+            stream->next_data_index++;
+        }
+    }
+    stream->buffer |= new_bytes >> stream->buffer_bits_filled;
+    stream->buffer_bits_filled += num_bytes_missing*8;
+}
+/**
+ * @brief ensure that the bitstream has at least n bits cached
+ * this function will fill the internal cache if the cache does not already have sufficient number of bits
+ * @param stream 
+ * @param n_bits 
+ */
+[[clang::always_inline,gnu::flatten]]
+static inline void BitStream_ensure_filled(
+    BitStream* const restrict stream,
+    const uint8_t n_bits
+){
+    if(stream->buffer_bits_filled<n_bits){
+        BitStream_fill_buffer(stream);
+    }
+}
+
+[[clang::always_inline,gnu::flatten]]
+static inline uint64_t BitStream_get_bits_unsafe(
+    const BitStream* const restrict stream,
+    uint8_t n_bits
+){
+    const uint64_t ret=stream->buffer>>(64-n_bits);
+    return ret;
+}
+
+/**
+ * @brief get next n bits from stream
+ * n must not be larger than 57. the internal bit buffer is automatically filled if it was not big enough at function start.
+ * this function does NOT advance the internal state, i.e. repeated calls to this function with the same arguments will yield the same result.
+ * call BitStream_advance to manually advance the stream.
+ * @param stream 
+ * @param n_bits 
+ * @return int 
+ */
+[[clang::always_inline,gnu::flatten]]
+static inline uint64_t BitStream_get_bits(
+    BitStream* const restrict stream,
+    const uint8_t n_bits
+){
+    BitStream_ensure_filled(stream, n_bits);
+
+    const uint64_t ret=BitStream_get_bits_unsafe(stream, n_bits);
+
+    return ret;
+}
+
 [[clang::always_inline,gnu::flatten,maybe_unused]]
 static inline uint8_t HuffmanCodingTable_lookup(
     const HuffmanCodingTable* const restrict table,
     BitStream* const restrict stream
 ){
-    uint64_t bits=BitStream_get_bits(stream,table->max_code_length_bits);
+    const uint64_t bits=BitStream_get_bits(stream,table->max_code_length_bits);
 
-    struct LookupLeaf leaf=table->lookup_table[bits];
+    const struct LookupLeaf leaf=table->lookup_table[bits];
     BitStream_advance_unsafe(stream, leaf.len);
 
     return leaf.value;
@@ -294,7 +284,7 @@ static inline uint64_t BitStream_get_bits_advance(
     BitStream* const restrict stream,
     const uint8_t n_bits
 ){
-    uint64_t res=BitStream_get_bits(stream, n_bits);
+    const uint64_t res=BitStream_get_bits(stream, n_bits);
     BitStream_advance_unsafe(stream, n_bits);
 
     return res;
